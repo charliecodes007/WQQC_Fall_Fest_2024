09aa449c18c9d933ac83c312601cb9de1ee3f5c7
Charlie Linville <charlie@benlaura.com>
2024-11-07 10:08:03 -0600

    more updates!

--- Project/qqms/main.py
+++ Project/qqms/main.py
@@ -1,15 +1,12 @@
 import numpy as np
-import qiskit
 from qiskit_experiments.library import T1
 from qiskit_ibm_runtime.fake_provider import FakeAthensV2
-from qiskit_ibm_runtime.fake_provider import FakeBrisbane
+from qiskit.providers.fake_provider import GenericBackendV2
 from qiskit_aer import AerSimulator
 from qiskit_aer.noise import NoiseModel
-import qiskit_aer
 import warnings
 from qiskit_ibm_runtime import QiskitRuntimeService
 from qiskit_experiments.library.characterization.t2hahn import T2Hahn
-import matplotlib.pyplot as plt
 import boto3
 from datetime import datetime
 
@@ -17,10 +14,9 @@ from datetime import datetime
 warnings.filterwarnings("ignore", message="Options .* have no effect in local testing mode.")
 
 
-def T1Test(numQubits, liveBackend=False, backend=None, customT1=False, customT1Data=None, delayStart=0, delayEnd=50,
-           delaySpread=100):
+def T1Test(numQubits, apiToken, liveBackend=False, backend=None, delayStart=0, delayEnd=50, delaySpread=100):
     if liveBackend is False:
-        backendProvider = FakeBrisbane()
+        backendProvider = GenericBackendV2(num_qubits=numQubits);
         #build noise model
         noise_model = NoiseModel.from_backend(
             backendProvider, gate_error=False, readout_error=False, thermal_relaxation=True)
@@ -29,17 +25,9 @@ def T1Test(numQubits, liveBackend=False, backend=None, customT1=False, customT1D
         simulator = AerSimulator.from_backend(backendProvider, noise_model=noise_model)
     else:
         #setup backend for nonlocal
-        service = QiskitRuntimeService(instance="ibm-q/open/main")
+        service = QiskitRuntimeService(token=apiToken, instance="ibm-q/open/main")
         simulator = service.backend(backend)
         #backendProvider = service
-
-    ##get known T1 data
-    #qubitT1Data = []
-    #if customT1 is False:
-    #    for i in range(numQubits):
-    #        qubitT1Data.append(backendProvider.qubit_properties(i).t1)
-    #else:
-    #    qubitT1Data = customT1Data
 
     #set delays for experiment
     delays = []
@@ -51,7 +39,7 @@ def T1Test(numQubits, liveBackend=False, backend=None, customT1=False, customT1D
     for i in range(numQubits):
         #create experiment
         experiment = T1(physical_qubits=(i,), delays=delays[i])
-        experiment.set_transpile_options(scheduling_method='asap')
+        experiment.set_transple_options(scheduling_method='asap')
 
         #run experiment
         experimentData = experiment.run(backend=simulator).block_for_results()
@@ -70,9 +58,9 @@ def T1Test(numQubits, liveBackend=False, backend=None, customT1=False, customT1D
     return experimentArray
 
 
-def T2Test(numQubits, liveBackend=False, backend=None, delayStart=0, delayEnd=50, delaySpread=100):
+def T2Test(numQubits, backend, apiToken, liveBackend=False, delayStart=0, delayEnd=50, delaySpread=100):
     if liveBackend is False:
-        backendProvider = FakeAthensV2()
+        backendProvider = GenericBackendV2(num_qubits=numQubits);
         #build noise model
         noise_model = NoiseModel.from_backend(
             backendProvider, gate_error=False, readout_error=False, thermal_relaxation=True)
@@ -80,7 +68,7 @@ def T2Test(numQubits, liveBackend=False, backend=None, delayStart=0, delayEnd=50
         simulator = AerSimulator.from_backend(backendProvider, noise_model=noise_model)
     else:
         #setup backend for nonlocal
-        service = QiskitRuntimeService(instance="ibm-q/open/main")
+        service = QiskitRuntimeService(token=apiToken, instance="ibm-q/open/main")
         simulator = service.backend(backend)
         #backendProvider = service
     #set delays
@@ -108,8 +96,13 @@ def T2Test(numQubits, liveBackend=False, backend=None, delayStart=0, delayEnd=50
     return experimentArray
 
 
-def addT1ToTable(tableName, QubitNum, T1Value, T1Std):
-    dynamodb = boto3.resource('dynamodb')
+def addT1ToTable(tableName, QubitNum, T1Value, T1Std, ACCESS_KEY, SECRET_KEY, REGION_NAME):
+    dynamodb = boto3.client(
+        'dynamodb',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name=REGION_NAME
+    )
     table = dynamodb.Table(tableName)
 
     timestamp = datetime.now()
@@ -124,8 +117,13 @@ def addT1ToTable(tableName, QubitNum, T1Value, T1Std):
     )
 
 
-def addT2ToTable(tableName, QubitNum, T2Value, T2Std):
-    dynamodb = boto3.resource('dynamodb')
+def addT2ToTable(tableName, QubitNum, T2Value, T2Std, ACCESS_KEY, SECRET_KEY, REGION_NAME):
+    dynamodb = boto3.client(
+        'dynamodb',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name=REGION_NAME
+    )
     table = dynamodb.Table(tableName)
 
     timestamp = datetime.now()
@@ -138,14 +136,3 @@ def addT2ToTable(tableName, QubitNum, T2Value, T2Std):
             'StandardDeviation': T2Std
         }
     )
-
-
-
-
-
-
-
-
-
-
-
